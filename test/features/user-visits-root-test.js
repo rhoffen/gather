39d237004164661c749fcf28c65d4215bbc239a6
rhoffenexitquitgit config --global user.name rhoffen <rebecca.hoffenberg@gmail.com>
2019-02-04 14:57:12 -0800

    First commit

---
+++ test/features/user-visits-root-test.js
@@ -0,0 +1,18 @@
+const {assert} = require('chai');
+
+describe('User visits root', () => {
+  describe('without existing items', () => {
+    it('starts blank', () => {
+      browser.url('/');
+      assert.equal(browser.getText('#items-container'), '');
+    });
+  });
+
+  describe('User clicks on "create" link', () => {
+    it('navigates to "create.html"',() => {
+      browser.url('/');
+      browser.click('a[href="create.html"]');
+      assert.equal(browser.getUrl(),'./create.html');
+    });
+  });
+});
