b761c23e9cfd0683e9f291a1664ba5a811433c3c
rhoffenexitquitgit config --global user.name rhoffen <rebecca.hoffenberg@gmail.com>
2019-02-13 17:58:21 -0800

    In the green at server level

--- test/features/user-visits-create-test.js
+++ test/features/user-visits-create-test.js
@@ -14,7 +14,7 @@ describe('User vists create page and posts a new item', () => {
       browser.setValue('#imageUrl-input', itemToCreate.imageUrl);
       browser.click('#submit-button');
       //verify
-      browser.url('/');
+      //browser.url('/');
       assert.include(browser.getText('body'), itemToCreate.title);
       //assert.include(brower.getText('body'), itemToCreate.description);
       //const img = $('body img')
