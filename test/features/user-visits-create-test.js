8faa99d37785fe959286b1a951c07dc64d7a892a
rhoffenexitquitgit config --global user.name rhoffen <rebecca.hoffenberg@gmail.com>
2019-02-21 10:36:29 -0800

    Commit before dropping to model level.

--- test/features/user-visits-create-test.js
+++ test/features/user-visits-create-test.js
@@ -14,13 +14,8 @@ describe('User vists create page and posts a new item', () => {
       browser.setValue('#imageUrl-input', itemToCreate.imageUrl);
       browser.click('#submit-button');
       //verify
-      //browser.url('/');
       assert.include(browser.getText('body'), itemToCreate.title);
-      //assert.include(brower.getText('body'), itemToCreate.description);
-      //const img = $('body img')
-      //const urlSrc = img.getAttribute('src');
       assert.include(browser.getAttribute('body img', 'src'), itemToCreate.imageUrl);
-
     });
 
 
