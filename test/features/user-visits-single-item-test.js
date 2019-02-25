5ddab2bbbe1f12490016a96d760d724bd1730d7b
rhoffenexitquitgit config --global user.name rhoffen <rebecca.hoffenberg@gmail.com>
2019-02-25 14:28:21 -0800

    Trying to fix single-item-view feature test.

--- test/features/user-visits-single-item-test.js
+++ test/features/user-visits-single-item-test.js
@@ -4,35 +4,20 @@ const itemToCreate = buildItemObject();
 
 // Add your tests below:
 describe('User visits single item view', () => {
-  describe('User vists create page and posts a new item', () => {
-    describe('clicks submit button', () => {
-      it('sees item rendered on main page', () => {
-        //setup
-        browser.url('/items/create');
-        //const itemToCreate = buildItemObject();
-        //exercise
-        browser.setValue('#title-input', itemToCreate.title);
-        browser.setValue('#description-input', itemToCreate.description);
-        browser.setValue('#imageUrl-input', itemToCreate.imageUrl);
-        browser.click('#submit-button');
-        //verify
-        browser.url('/');
-        assert.include(browser.getText('body'), itemToCreate.title);
-        assert.include(browser.getAttribute('body img', 'src'), itemToCreate.imageUrl);
-      });
+    it('shows item description', () => {
+    //setup - visit create page and submit new item
+    browser.url('/items/create');
+    browser.setValue('#title-input', itemToCreate.title);
+    browser.setValue('#description-input', itemToCreate.description);
+    browser.setValue('#imageUrl-input', itemToCreate.imageUrl);
+    browser.click('#submit-button');
     });
-  });
-  describe('User clicks on view button', () => {
-    it('Shows item description', () => {
-        //browser.url('/');
-        console.log('**body of / :' + browser.getText('body'));
-        console.log('***itemToCreate = ' + itemToCreate);
-        browser.click('.view-button a');
-        console.log('body after clicking view button: ' + browser.getText('body'));
-        //const createdItem = Item.findOne(itemToCreate);
-        //browser.url('/items/' + createdItem._id);
-        //browser.url('single');
-        //assert.include(browser.getText('body'), createdItem.description);
-    });
-  });
+    //exercise - click on view button from main page
+    console.log('**body of / :' + browser.getText('body'));
+    console.log('***itemToCreate = ' + itemToCreate);
+    browser.click('.view-button a');
+    console.log('body after clicking view button: ' + browser.getText('body'));
+    //verify
+    console.log('&&&itemToCreate.description = ' + itemToCreate.description);
+    assert.include(browser.getText('body'), itemToCreate.description);
 });
